4937e1142989bfb2715351bb95d962bc7ce41b14
dilstar <partinerlover@gmail.com>
2025-06-11 22:29:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -61,3 +61,25 @@ client.on('message.upsert', async (m) => {
     react(client, msg);
   }
 });
+
+// autoread.js
+module.exports = async (client, message) => {
+  try {
+    await client.readMessages([message.key]);
+  } catch (e) {
+    console.log('AutoRead Error:', e);
+  }
+};
+```
+
+```js
+// index.js
+const autoread = require('./commands/autoread.js');
+
+client.on('message.upsert', async (m) => {
+  const msg = m.messages[0];
+  if (!msg.message) return;
+  if (msg.key.fromMe) return;
+
+  autoread(client, msg);
+});
